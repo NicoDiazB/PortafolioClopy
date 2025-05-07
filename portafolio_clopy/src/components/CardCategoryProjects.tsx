a4ac290ca1928dfcedeb5db16fad76d8b0426260
NicoDiaz <nicolas.diaz.bn@gmail.com>
2025-05-07 13:24:05 -0500

    cambio en las cards principales

--- portafolio_clopy/src/components/CardCategoryProjects.tsx
+++ portafolio_clopy/src/components/CardCategoryProjects.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 export const CardCategoryProjects: React.FC<CardProjectProps> = ({ title, imageUrl }) => {
   return (
-    <div className="relative w-full max-w-sm h-90 rounded-lg overflow-hidden group cursor-pointer shadow-md">
+    <div className="relative h-fit max-w-lg rounded-lg  group cursor-pointer ">
       <img
         src={imageUrl}
         alt={title}
-        className="w-full h-full transition-opacity duration-300 group-hover:opacity-40"
+        className=" transition-opacity duration-300 group-hover:opacity-50 "
         loading='lazy'
         />
-        <p className='absolute bottom-5 left-25 z-50 text-xl'>Click on the image</p>
+        <p className='absolute bottom-5 left-10 z-50 text-lg font-bold'>Click on the image</p>
       <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         <span className="text-black text-2xl font-bold drop-shadow-md text-center px-4">{title}</span>
       </div>
