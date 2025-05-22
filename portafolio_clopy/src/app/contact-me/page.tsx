d65336da6c6dee25e54583caf4c291dc2c69d05c
NicoDiaz <nicolas.diaz.bn@gmail.com>
2025-05-22 14:59:02 -0500

    se agrego publications

--- portafolio_clopy/src/app/contact-me/page.tsx
+++ portafolio_clopy/src/app/contact-me/page.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 function ContactMe() {
   return (
-    <div className='pt-40 md:pt-60'>
+    <section className='pt-40 md:pt-60'>
         <h1 className='text-4xl font-bold text-center '>Contact Me</h1>
         <p className='text-center text-lg px-4'>I am always open to discussing new projects, creative ideas or opportunities to be part of your vision.</p>
         <div className='flex flex-col items-center justify-center '>
           <FormContact/>
         </div>
-    </div>
+    </section>
   )
 }
 
