df1123decc84cd9323e87b5bb74d6c95ee34ed4f
NicoDiaz <nicolas.diaz.bn@gmail.com>
2025-05-27 17:09:03 -0500

    se corrigio el punteado y es epescaiado de la section de arriba del home

--- portafolio_clopy/src/app/page.tsx
+++ portafolio_clopy/src/app/page.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center  items-center ">
-      <section className="flex w-full justify-end pt-4 pr-2">
-        <Link className="font-bold min-w-[100px] md:min-w-[120px] text-center  md:text-xl text-[#4B70A6] border-b-3 border-dashed hover:text-black" href={"/contact-me"}> Curious? Let’s talk</Link>
-        <Link className="font-bold min-w-[100px] md:min-w-[120px] text-center  md:text-xl text-[#4B70A6] border-b-3 border-dashed hover:text-black" href={"/contact-me"}> CV</Link>
+      <section className="flex w-full justify-evenly  md:justify-end pt-4 md:gap-10 md:pr-6">
+        <Link className="font-bold min-w-[60px] md:min-w-[80px] text-center  md:text-xl text-[#4B70A6] border-b-3 border-dashed hover:text-black" href={"/contact-me"}> Curious? Let’s talk</Link>
+        <Link className="font-bold min-w-[60px] md:min-w-[80px] text-center  md:text-xl text-[#4B70A6] border-b-3 border-dashed hover:text-black" href={"/contact-me"}> CV</Link>
       </section>
      <section className="flex flex-col items-center md:max-w-[1000px] gap-4 mt-30  ">
-      <h1 className="font-bold text-2xl  md:text-4xl ">Hi, i am Clopy</h1>
+      <h1 className="font-bold text-2xl  md:text-4xl ">Hi, I am Clopy</h1>
       <p className="text-lg text-center px-4 md:text-2xl  leading-tight ">I’m a multidisciplinary design researcher and artist. I work with fibres, stories, and participatory methods to explore and make sense of social and community needs. Collaboration and dialogue are at the heart of my process, interpreting them as ways to understand and weave connections among people, places, and ideas to contribute towards positive change. See my work below!</p>
      </section>
 
@@ -32,7 +32,7 @@ export default function Home() {
         </Link>
       </div>
 
-      <div className="gap-4 md:gap-8 flex flex-wrap justify-center items-center ">
+      <div className="gap-20 md:gap-26 flex flex-wrap justify-center items-center ">
         <Link className=" font-bold min-w-[100px] md:min-w-[120px] text-center  md:text-xl border-b-3 text-[#4B70A6] border-dashed hover:text-black" href={"/exhibition"}>
           Exhibitions
         </Link>
